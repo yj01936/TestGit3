26f7e797cdad90cdf5aaf747e6a5f6ac0d78032f
yanjing_glb <yanjing_glb@sohu.com>
2019-12-09 11:17:11 +0800

    test hot_fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
  public static void main(String[] args) {
 	System.out.println("right commit test!");
 	System.out.println("left commit test...!");
+	System.out.println("hot_fix test...!");
 }
 }
